b54ea57f7772005c7c686bcc2b62a559c7df5ba2
Fernando Otero <febo@tensor.so>
2024-04-20 09:33:59 +0100

    Add u8 AVL tree type (#1)

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -1,7 +1,10 @@
 pub mod avl_tree;
 pub mod hash_set;
+pub mod u8_avl_tree;
 
 pub use avl_tree::AVLTree;
 pub use avl_tree::AVLTreeMut;
 pub use hash_set::HashSet;
 pub use hash_set::HashSetMut;
+pub use u8_avl_tree::U8AVLTree;
+pub use u8_avl_tree::U8AVLTreeMut;
